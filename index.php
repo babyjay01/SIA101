7c9ae45ecef3b375210d2a7d08bb0cecb6e0373b
root <root@DESKTOP-086RQDJ.localdomain>
2021-12-10 13:55:56 +0800

    php Installation

---
+++ index.php
@@ -0,0 +1,18 @@
+  <?php
+                echo"Hello Mabuhay! My Name is Bebie john D. Atol";
+        <html>
+                <body>
+                <h1>PHP7 and Apache Inastallation</h1>
+                <p> First you need to operate the windows features enable the WSL also check the Vrtual Machine. further more guide i will providing the url below also check the video in the archive google class in the sub of SA101</p>
+
+                <p> Installation php first access the administration using the command promp afterwards install the php7.
+
+                </p>
+                <a href= "https://docs.microsoft.com/en-us/windows/wsl/install-manual#step-4---download-the-linux-kernel-update-package"></a>
+                </body>
+                </html>
+?>
+
+:wq
+
+
