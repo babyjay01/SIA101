230a95964a37b7b85770d1c8389babefbc73dac4
babyjay01 <atolbebiejohn@gmail.com>
2021-12-10 18:15:20 +0800

    edited

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
   <?php
                 echo"Hello Mabuhay! My Name is Bebie john D. Atol";
-        <html>
-                <body>
-                <h1>PHP7 and Apache Inastallation</h1>
-                <p> First you need to operate the windows features enable the WSL also check the Vrtual Machine. further more guide i will providing the url below also check the video in the archive google class in the sub of SA101</p>
+        
+                
+                echo "PHP7 and Apache Inastallation"; 
+                echo" First you need to operate the windows features enable the WSL also check the Vrtual Machine. further more guide i will providing the url below also check the video in the archive google class in the sub of SA101";
 
-                <p> Installation php first access the administration using the command promp afterwards install the php7.
+		echo " Installation php first access the administration using the command promp afterwards install the php7";
 
-                </p>
-                <a href= "https://docs.microsoft.com/en-us/windows/wsl/install-manual#step-4---download-the-linux-kernel-update-package"></a>
-                </body>
-                </html>
+                
+                
+                
 ?>
 
-:wq
+
 
 
